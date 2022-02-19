4f7fb7e0faf13d917cbc109260f91568220f4e06
SunnyWorker <93293748+SunnyWorker@users.noreply.github.com>
2022-02-19 11:19:52 +0300

    Update Task6.sh

--- Task6.sh
+++ Task6.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-mypath() {
-
-	for file in `find $2 -maxdepth 1 -name "*.$3"`
-	do
-		echo "`basename $file`" >> $1
-	done
-
-	return 0
-}
-
 
 if [ -f "$1" ] && [ $# -eq 3 ]
 then
 	echo > $1
-	mypath $1 $2 $3
+	for file in `find $2 -maxdepth 1 -name "*.$3"`
+	do
+		echo "`basename $file`" >> $1
+	done
 	sort -o $1 $1
 else
 	echo "Wrong count of arguments or Output way is not file"
